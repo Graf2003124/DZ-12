1c4a4599406733fd93d9f2ccd8fb833b507ee9b0
grafo <grafonov2003@inbox.ru>
2024-06-28 12:53:34 +0500

    Ne rabotaet HELLP

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Author Esenin = new Author("Sergey", "Esenin");
+        Author Tolstoy = new Author("Lev", "Tolstoy");
+        Book VoinaIMir = new Book("VoinaIMir", Tolstoy, 1860);
+        Book PismoKJenjene = new Book("PismoKJenjene", Esenin, 2012);
+        VoinaIMir.setYearOfPublishing(1867);
+        System.out.println(VoinaIMir);
+        System.out.println(PismoKJenjene);
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,28 @@
 public class Book {
+    private String title;
+    private Author author;
+    private int yearOfPublishing;
+
+    public Book(String title, Author author, int yearOfPublishing) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
 }
+
